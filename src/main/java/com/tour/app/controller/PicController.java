fa2a05405ca524c5fcb3fbc8c5f792bb44634ad7
lyq <aaa>
2019-05-03 17:15:33 +0800

    5.3.1

--- src/main/java/com/tour/app/controller/PicController.java
+++ src/main/java/com/tour/app/controller/PicController.java
@@ -1,9 +1,34 @@
 package com.tour.app.controller;
 
+import com.tour.app.model.entity.Pic;
+import com.tour.app.model.mapper.PicMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/pic")
 public class PicController {
 
+    @Autowired
+    PicMapper picMapper;
+
+    @GetMapping("/add")
+    public String add(){
+
+        return "pic/addPic";
+    }
+
+    @ResponseBody
+    @GetMapping("/pid/{id}")
+    public Object selectByPid(@PathVariable("id")Integer id){
+
+
+        List<Pic> pics = picMapper.selectPic(id);
+
+        return pics;
+    }
 
 }
--- src/main/java/com/tour/app/model/mapper/PicMapper.java
+++ src/main/java/com/tour/app/model/mapper/PicMapper.java
@@ -1,12 +1,13 @@
 package com.tour.app.model.mapper;
 
+import com.tour.app.model.entity.Pic;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface PicMapper {
 
-
-
-
+    List<Pic> selectPic(Integer pid);
 
 }
